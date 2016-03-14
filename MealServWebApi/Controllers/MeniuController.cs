92173c595d78fad635f20b31fe930325c3edf1b6
Sacerdotianu George <george_sacerdotianu@hotmail.com>
2016-03-14 10:20:13 +0200

    Initial commit

---
+++ MealServWebApi/Controllers/MeniuController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MealServWebApi.Models;
+
+namespace MealServWebApi.Controllers
+{
+    public class MeniuController : ApiController
+    {
+        Meniu[] menus = new Meniu[] 
+        { 
+            new Meniu { Id = 1, Name = "Ciorba perisoare", Category = "Fel1", Price = 1 }, 
+            new Meniu { Id = 2, Name = "Pizza", Category = "Fel2", Price = 3.75M }, 
+            new Meniu { Id = 3, Name = "Savarina", Category = "Desert", Price = 16.99M } 
+        };
+
+        public IEnumerable<Meniu> GetAllMenu()
+        {
+            return menus;
+        }
+
+        public IHttpActionResult GetMenu(int id)
+        {
+            var menu = menus.FirstOrDefault((p) => p.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+            return Ok(menu);
+        }
+    }
+}
